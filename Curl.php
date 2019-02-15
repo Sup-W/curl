d7004e36dccdd57bb8a2398498e168d27ec13f6b
wyman <67239077@qq.com>
2019-02-15 14:18:34 +0800

    支持并发多个不同链接

--- Curl.php
+++ Curl.php
@@ -4,19 +4,20 @@
 /**
  * Created by PhpStorm.
  * User: ThinkPad
- * Date: 2018/10/23
+ * Date: 2019/2/15
  * Time: 11:36
- * @method get ($url, $data = array(), $option = array())
- * @method post ($url, $data = array(), $option = array())
+ * @method get ($data = array(), $option = array())
+ * @method post ($data = array(), $option = array())
  * @return array single->array('success'=>true,'code'=>200, 'msg'=>$res) multi->array('success'=>true,'number'=>'777888','code'=>200, 'msg'=>$res)
  * sample:
- * $curl->method(Curl::MULTI_REQUEST)->post($url,$data,$option);
- * $curl->post($url,$data,$option);
+ * $curl->url("http://www.baidu.com")->method(Curl::MULTI_REQUEST)->post($data,$option);
+ * $curl->post($data,$option);
  */
 class Curl
 {
-    const MULTI_REQUEST = 'multi';
-    const SINGLE_REQUEST = 'single';
+    const MULTI_REQUEST = '_multi';
+    const SINGLE_REQUEST = '_single';
+
     /**
      * curl配置内容
      * @var array
@@ -32,7 +33,8 @@ class Curl
      * @var array
      */
     private $_data = array();
-    private $type = self::SINGLE_REQUEST;
+    private $_type = self::SINGLE_REQUEST;
+    private $_url = '';
 
 
     /**
@@ -44,9 +46,9 @@ class Curl
     public function __call($method, $args)
     {
         //设置数据
-        $this->_data = $args[1] ?: array();
-        $this->_prepareParams($args[0], $args[2] ?: array());
-        return $this->_request($this->type, $method);
+        $this->_data = $args[0] ?: array();
+        $this->_prepareParams($args[1] ?: array());
+        return $this->_request($this->_type, $method);
     }
 
     /**
@@ -56,9 +58,16 @@ class Curl
      */
     public function method($type)
     {
-        $this->type = $type;
+        $this->_type = $type;
         return $this;
     }
+
+    public function url($url)
+    {
+        $this->_url = $url;
+        return $this;
+    }
+
 
     /**
      * 组织传入的参数
@@ -66,23 +75,18 @@ class Curl
      * @param $option
      * @return bool
      */
-    private function _prepareParams($url, $option)
+    /**
+     * @param $option
+     */
+    private function _prepareParams($option)
     {
-        if (is_string($url) && strlen($url)) {
-            $this->_options[CURLOPT_URL] = $url;
-        } else {
-            return false;
-        }
         //设置参数
         if ($option) {
             foreach ($option as $k => $v) {
                 $this->_options[$k] = $v;
             }
         }
-        if (stripos($url, "https://") !== FALSE) {
-            $this->_options[CURLOPT_SSL_VERIFYPEER] = false;
-            $this->_options[CURLOPT_SSL_VERIFYHOST] = false;
-        }
+
     }
 
 
@@ -103,7 +107,7 @@ class Curl
      * @param $method
      * @return array|mixed
      */
-    private function single($method)
+    private function _single($method)
     {
 
         $mh = curl_init();
@@ -127,7 +131,7 @@ class Curl
      * @param $method
      * @return array
      */
-    private function multi($method)
+    private function _multi($method)
     {
 
         $mh = curl_multi_init(); //1 创建批处理cURL句柄
@@ -181,13 +185,24 @@ class Curl
      */
     private function _setMethod($method, $data = array())
     {
+        //设置安全认证为不可以用
+        if (isset($data['url']) && stripos($data['url'], "https://") !== FALSE) {
+            $this->_options[CURLOPT_SSL_VERIFYPEER] = false;
+            $this->_options[CURLOPT_SSL_VERIFYHOST] = false;
+        }
+
+        //如果是每个独立链接，参数中需要去掉对应的链接值
         switch (strtolower($method)) {
             case 'post':
+                $this->_options[CURLOPT_URL] = $this->_url ?: $data['url'];
+                unset($data['url']);
                 $this->_options[CURLOPT_POST] = true;
                 count($data) ? $this->_options[CURLOPT_POSTFIELDS] = http_build_query($data) : null;
                 break;
             case 'get':
-                $this->_options[CURLOPT_URL] = count($data) ? $this->_options[CURLOPT_URL] . '?' . http_build_query($data) : $this->_options[CURLOPT_URL];
+                $url = ($this->_url ?: $data['url']);
+                unset($data['url']);
+                $this->_options[CURLOPT_URL] = count($data) ? $url . '?' . http_build_query($data) : $url;
                 break;
             default:
                 return false;
